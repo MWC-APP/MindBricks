091be043ea9fea63fcebf9ce60bfe220d0d1e1d2
Luca Di Bello <lucadibello@proton.me>
2025-12-01 16:32:20 +0100

    feat: now SessionLog POJO registered in Room database context

--- android/app/src/main/java/ch/inf/usi/mindbricks/database/AppDatabase.java
+++ android/app/src/main/java/ch/inf/usi/mindbricks/database/AppDatabase.java
@@ -6,17 +6,19 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import ch.inf.usi.mindbricks.model.SessionSensorLog;
 import ch.inf.usi.mindbricks.model.StudySession;
 
 /**
  * Room database for MindBricks app
  */
-@Database(entities = {StudySession.class}, version = 1, exportSchema = false)
+@Database(entities = {StudySession.class, SessionSensorLog.class}, version = 2, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
 
     private static AppDatabase INSTANCE;
 
     public abstract StudySessionDao studySessionDao();
+    public abstract SessionSensorLogDao sessionSensorLogDao();
 
     public static synchronized AppDatabase getInstance(Context context) {
         if (INSTANCE == null) {
