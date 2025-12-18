8bec10ed46dbda43a2de45704c1baad713752283
Luca Di Bello <lucadibello@proton.me>
2025-12-18 22:23:26 +0100

    fix: forgot to push important file

---
+++ android/app/src/main/java/ch/inf/usi/mindbricks/model/plan/DayKey.java
@@ -0,0 +1,55 @@
+package ch.inf.usi.mindbricks.model.plan;
+
+/**
+ * Enum representing the days of the week.
+ *
+ * @author Luca Di Bello
+ */
+public enum DayKey {
+    MONDAY("monday", (short) 0),
+    TUESDAY("tuesday", (short) 1),
+    WEDNESDAY("wednesday", (short) 2),
+    THURSDAY("thursday", (short) 3),
+    FRIDAY("friday", (short) 4),
+    SATURDAY("saturday", (short) 5),
+    SUNDAY("sunday", (short) 6);
+
+    /**
+     * Unique key for each day.
+     */
+    private final String key;
+
+    /**
+     * Index of the day in the week where Monday is 0, and Sunday is 6.
+     */
+    private final short index;
+
+    /**
+     * Constructor for DayKey.
+     *
+     * @param key Unique key for each day
+     * @param index Index of the day in the week where Monday is 0, and Sunday is 6
+     */
+    DayKey(String key, short index) {
+        this.key = key;
+        this.index = index;
+    }
+
+    /**
+     * Get the unique key for the day.
+     *
+     * @return Unique key for the day
+     */
+    public String getKey() {
+        return key;
+    }
+
+    /**
+     * Get the index of the day in the week.
+     *
+     * @return Index of the day in the week
+     */
+    public short getIndex() {
+        return index;
+    }
+}
